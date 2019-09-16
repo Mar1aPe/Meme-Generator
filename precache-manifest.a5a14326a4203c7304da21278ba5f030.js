18160e3343080bff28d102af4d93dfed6688e9ec
Mar1aPe <adresmarii@gmail.com>
2019-09-17 00:10:44 +0200

    Updates

---
+++ precache-manifest.a5a14326a4203c7304da21278ba5f030.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a83b864631454879103fdf63bcde822",
+    "url": "/Meme-Generator/index.html"
+  },
+  {
+    "revision": "104341aa930fda328264",
+    "url": "/Meme-Generator/static/css/main.3f659267.chunk.css"
+  },
+  {
+    "revision": "6cab810ea48b6ddb33d9",
+    "url": "/Meme-Generator/static/js/2.cbd62256.chunk.js"
+  },
+  {
+    "revision": "104341aa930fda328264",
+    "url": "/Meme-Generator/static/js/main.171841fa.chunk.js"
+  },
+  {
+    "revision": "aae5e831a893920e61f7",
+    "url": "/Meme-Generator/static/js/runtime~main.e0549e7e.js"
+  }
+]);
